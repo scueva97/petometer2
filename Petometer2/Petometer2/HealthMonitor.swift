99b782ff941dfa8bca6d0872be3da47e206f159c
Sebastian Cueva <sc3972@columbia.edu>
2016-02-05 22:25:28 -0500

    Blank files

---
+++ Petometer2/Petometer2/HealthMonitor.swift
@@ -0,0 +1,15 @@
+//
+//  HealthMonitor.swift
+//  Petometer2
+//
+//  Created by Sebastian Cueva-Caro on 2/5/16.
+//  Copyright © 2016 Opal. All rights reserved.
+//
+
+import Foundation
+
+class HealthMonitor {
+    
+    
+    
+}
