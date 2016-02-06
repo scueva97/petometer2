392bc3417a47b1919e854f322f4ccc479bef1978
Sebastian Cueva <sc3972@columbia.edu>
2016-02-06 08:30:03 -0500

    something is working with gifs

--- Petometer2/Petometer2/ViewController.swift
+++ Petometer2/Petometer2/ViewController.swift
@@ -16,12 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var steps: UILabel!
     
     
-    @IBOutlet weak var stateImageView: AnimatableImageView!
     var days:[String] = []
     var stepsTaken:[Int] = []
     
-        
-
+    @IBOutlet weak var stateImageView: UIImageView!
     let activityManager = CMMotionActivityManager()
     let pedoMeter = CMPedometer()
     
@@ -42,43 +40,61 @@ class ViewController: UIViewController {
         
         
         self.activityState.text = "Get up and move!"
-        self.stateImageView.animateWithImage(named: "EvolShake.gif") //change this to an egg
+        self.stateImageView.image = UIImage(named: "Egg2.png")//change this to an egg
         
-        //array of stationary gif
-        //let stationaryList = ["Stationary.gif","Stationary2.gif","Stationary3.gif","Stationary4.gif","Stationary5.gif","Stationary6.gif","Stationary7.gif"]
+        /**
+        array of stationary gif
+        let stationaryList = ["Stationary.gif","Stationary2.gif","Stationary3.gif","Stationary4.gif","Stationary5.gif","Stationary6.gif","Stationary7.gif"]
         
-        //array of walking gif
-        //let walkingList = ["Walk.gif","Walk2.gif","Walk3.gif","Walk4.gif","Walk5.gif","Walk6.gif","Walk7.gif"]
+        array of walking gif
+        let walkingList = ["Walk.gif","Walk2.gif","Walk3.gif","Walk4.gif","Walk5.gif","Walk6.gif","Walk7.gif"]
         
-        //array of running gif
-        //let runningList = ["Run.gif","Run2.gif","Run3.gif","Run4.gif","Run5.gif","Run6.gif","Run7.gif"]
-        
+        array of running gif
+        let runningList = ["Run.gif","Run2.gif","Run3.gif","Run4.gif","Run5.gif","Run6.gif","Run7.gif"]
+        */
+
         if(CMMotionActivityManager.isActivityAvailable()){
             self.activityManager.startActivityUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { (data: CMMotionActivity?) -> Void in
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    
                     if(data!.stationary == true){
                         self.activityState.text = "Stationary"
-                        //randomizer
-                        //let randomNumber = Int(arc4random_uniform(8))
-                        //let gifName = stationaryList[randomNumber]
-                        //self.stateImageView.animateWithImage(named: gifName)
-                        self.stateImageView.animateWithImage(named: "Stationary6.gif")
+                        
+                        self.stateImageView.image = UIImage(named: "StationaryStill.png")
+                        /**
+                        randomizer
+                        let randomNumber = Int(arc4random_uniform(8))
+                        let gifName = stationaryList[randomNumber]
+                        self.stateImageView.animateWithImage(named: gifName)
+                        self.stateImageView.animateWithImage(named: "StationaryStill.png")
+                        */
                     } else if (data!.walking == true){
                         self.activityState.text = "Walking"
-                        //randomizer
-                        //let randomNumber = Int(arc4random_uniform(8))
-                        //let gifName = walkingList[randomNumber]
-                        //self.stateImageView.animateWithImage(named: gifName)
-                        self.stateImageView.image = UIImage(named: "Walk6.gif")
+                        
+                        self.stateImageView.image = UIImage(named: "WalkStill.png")
+                        /**
+                        randomizer
+                        let randomNumber = Int(arc4random_uniform(8))
+                        let gifName = walkingList[randomNumber]
+                        self.stateImageView.animateWithImage(named: gifName)
+                        self.stateImageView.image = UIImage(named: "WalkStill.png")
+                        */
                     } else if (data!.running == true){
                         self.activityState.text = "Running"
-                        //randomizer
-                        //let randomNumber = Int(arc4random_uniform(8))
-                        //let gifName = runningList[randomNumber]
-                        //self.stateImageView.animateWithImage(named: gifName)
-                        self.stateImageView.image = UIImage(named: "Run3.gif")
-                    } else if (data!.automotive == true){
-                        self.activityState.text = "Automotive"
+                        
+                        self.stateImageView.image = UIImage(named: "RunStill.png")
+
+                        /**
+                        randomizer
+                        let randomNumber = Int(arc4random_uniform(8))
+                        let gifName = runningList[randomNumber]
+                        self.stateImageView.animateWithImage(named: gifName)
+                        self.stateImageView.image = UIImage(named: "RunStill.png")
+                        */
+//                    } else if (data!.automotive == true){
+//                        self.activityState.text = "Automotive"
+//                    }
                     }
                 })
                 
@@ -87,7 +103,6 @@ class ViewController: UIViewController {
         if(CMPedometer.isStepCountingAvailable()){
             let fromDate = NSDate(timeIntervalSinceNow: -86400 * 7)
             self.pedoMeter.queryPedometerDataFromDate(fromDate, toDate: NSDate()) { (data : CMPedometerData?, error) -> Void in
-                print(data)
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     if(error == nil){
                         self.steps.text = "\(data!.numberOfSteps)" 
---
+++ Petometer2/Petometer2/WorkoutListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  WorkoutListViewController.swift
+//  Petometer2
+//
+//  Created by Sebastian Cueva-Caro on 2/6/16.
+//  Copyright © 2016 Opal. All rights reserved.
+//
+
+import UIKit
+import Gifu
+import CoreMotion
+
+class WorkoutListViewController: UIViewController {
+
+    @IBOutlet weak var workoutImage: AnimatableImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.workoutImage.animateWithImage(named: "Run7.gif")
+        //workoutImage.startAnimatingGIF()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
